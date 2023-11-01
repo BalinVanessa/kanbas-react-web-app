43014ad56dae425580436b4a5493c2ec22e12d0d
BalinVanessa <vanashbalin@gmail.com>
2023-10-31 23:15:01 -0400

    A4 Lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -5,11 +5,20 @@ import PathParameters from "./PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoItem from "./todos/ToDoItem";
 import TodoList from "./todos/ToDoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+    const { todos } = useSelector((state) => state.todosReducer);
     return (
         <div class="container">
             <h1>Assignment 3</h1>
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
             <TodoItem />
             <TodoList />
             <ConditionalOutput />
