2866244734158f4c193c020b9b05fce43dec408b
BalinVanessa <vanashbalin@gmail.com>
2023-11-16 00:11:32 -0500

    Updated Environment Variables

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -18,7 +18,7 @@ function Kanbas() {
         startDate: "2023-09-10", endDate: "2023-12-15",
     });
 
-    const URL = "http://localhost:4000/api/courses";
+    const URL = `${API_BASE}/courses`;
     const findAllCourses = async () => {
         const response = await axios.get(URL);
         setCourses(response.data);
